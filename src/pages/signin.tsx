bfdccdbc65860fbaf90489c7f838797b1d27ecaa
Md. Al Amin Chowdhury <94056482+Alamin031@users.noreply.github.com>
2023-09-25 18:34:34 +0600

    BS1433

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,26 +1,47 @@
 import Link from 'next/link';
 import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
 
 const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
+        firstName: data.get('firstName'),
+        lastName: data.get('lastName'),
         email: data.get('email'),
     });
 }
 
-function login() {
+function signin() {
     return (
         <div className="flex p-0 items-center justify-center min-h-screen bg-gray-900">
             <form className="p-0" onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-3 self-stretch p-0">
                     <h2 className="text-2xl text-white justify-center font-semibold mb-4 text-center">Chatbot-23</h2>
-                    <h2 className="text-2xl text-white justify-center font-semibold mb-4 text-center">Sign in to your account</h2>
+                    <h2 className="text-2xl text-white justify-center font-semibold mb-4 text-center">Sign up with your details</h2>
 
                     <div className='flex flex-col gap-3'>
-                        <label className=' text-white text-sm font-bold' htmlFor='email'>  
+                        <label className='text-white text-sm font-bold' htmlFor='firstName'>  
+                            First Name
+                            <input
+                                id='firstName'
+                                type='text'
+                                name='firstName'
+                                placeholder='Enter First Name'
+                                className='shadow mt-3 border-gray-500 appearance-none border rounded w-full py-2 px-3 text-white bg-gray-900 leading-tight focus:outline-none focus:shadow-outline'
+                            />
+                        </label>
+                        <label className='text-white text-sm font-bold' htmlFor='lastName'>  
+                            Last Name
+                            <input
+                                id='lastName'
+                                type='text'
+                                name='lastName'
+                                placeholder='Enter Last Name'
+                                className='shadow mt-3 border-gray-500 appearance-none border rounded w-full py-2 px-3 text-white bg-gray-900 leading-tight focus:outline-none focus:shadow-outline'
+                            />
+                        </label>
+                        <label className='text-white text-sm font-bold' htmlFor='email'>  
                             Email Address
                             <input
                                 id='email'
@@ -33,35 +54,19 @@ function login() {
                         <button
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">       
-                            Proceed
+                            Next
                         </button>
                     </div>  
                     <p className="mt-4 text-sm text-center text-gray-500">
-                        Not a user?{' '}
+                        Already a user?{' '}
                         <Link href="/" className="text-blue-500 underline">
-                            Sign up here
+                            Sign in here
                         </Link>
                     </p>
-                    <p className="mt-2 text-sm text-center">
-                        <Link href="/" className="text-blue-500 underline">
-                            Forgot Password?
-                        </Link>
-                    </p>
-                    <div className="flex items-center justify-center text-sm font-medium leading-6">
-                        <div className="bg-white h-px flex-grow" />
-                        <span className="text-white px-4">Or</span>
-                        <div className="bg-white h-px flex-grow" />
-                    </div>                    
-                    <button
-                        type="button"
-                        className="bg-mainColor flex bg-white justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                    >
-                        <FcGoogle className="mr-4" /> Sign in with Google
-                    </button>
                 </div>
             </form>
         </div>
     );
 }
 
-export default login;
+export default signin;
